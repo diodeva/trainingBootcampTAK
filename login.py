012e65d082e64dc8250587b062dab31a9281efad
diodev <devlinedio@gmail.com>
2023-03-13 12:40:09 +0700

    update validation 1

--- login.py
+++ login.py
@@ -63,32 +63,23 @@ class TestLogin(unittest.TestCase):
         driver.find_element(By.CLASS_NAME, "ico-login").click()
         driver.find_element(By.CLASS_NAME, "forgot-password").click()
         driver.find_element(By.ID, "Email").send_keys("superQA@mailto.com")
+        driver.find_element(By.NAME, "send-email").click()
+
+    def test_success_recovery(self):
+        driver = self.browser
+        driver.get("https://demowebshop.tricentis.com/")
+        driver.find_element(By.CLASS_NAME, "ico-login").click()
+        driver.find_element(By.CLASS_NAME, "forgot-password").click()
+        driver.find_element(By.ID, "Email").send_keys("superQA@gmail.com")
+        driver.find_element(By.NAME, "send-email").click()
         
-        # wait = WebDriverWait(driver, 10)
-        # button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="send-email"].button-1.password-recovery-button')))
-        # button.click()
-        driver.find_element(By.CLASS_NAME, 'button-1 password-recovery-button').click()
-
-
-    # def test_success_recovery(self):
-    #     driver = self.browser
-    #     driver.get("https://demowebshop.tricentis.com/")
-    #     driver.find_element(By.CLASS_NAME, "ico-login").click()
-    #     driver.find_element(By.CLASS_NAME, "forgot-password").click()
-    #     driver.find_element(By.ID, "Email").send_keys("superQA@gmail.com")
-
-    #     wait = WebDriverWait(driver, 10)
-    #     button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="send-email"].button-1.password-recovery-button')))
-    #     button.click()
-    #     # driver.find_element(By.CSS_SELECTOR, 'input[name="send-email"].button-1.password-recovery-button').click()
-
-    # def test_success_login(self):
-    #     driver = self.browser
-    #     driver.get("https://demowebshop.tricentis.com/")
-    #     driver.find_element(By.CLASS_NAME, "ico-login").click()
-    #     driver.find_element(By.ID, "Email").send_keys("superQA@gmail.com")
-    #     driver.find_element(By.ID, "Password").send_keys("super123")
-    #     driver.find_element(By.CLASS_NAME, "button-1.login-button").click()
+    def test_success_login(self):
+        driver = self.browser
+        driver.get("https://demowebshop.tricentis.com/")
+        driver.find_element(By.CLASS_NAME, "ico-login").click()
+        driver.find_element(By.ID, "Email").send_keys("superQA@gmail.com")
+        driver.find_element(By.ID, "Password").send_keys("super123")
+        driver.find_element(By.CLASS_NAME, "button-1.login-button").click()
 
 
     def tearDown(self):
